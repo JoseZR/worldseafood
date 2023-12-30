d7a502bf8a0f1ecdd7f991b316e2dc343ae67d50
Jesus <josezerrod@hotmail.com>
2023-12-30 02:17:28 -0600

    se integra sistema de traducción i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
+
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://hfmexico.mx/worldseafood/",
+  base: "/worldseafood",
+  packageOptions: {
+    // Set type to module for ECMAScript module support
+    type: 'module',
+    knownEntrypoints: ['@astrojs/partytown'],
+  },    
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "es",
+      locales: {
+        en: "en",
+        es: "es"
+      }
+    }
+  }), tailwind(), astroI18next(), react(), partytown()]
 });
