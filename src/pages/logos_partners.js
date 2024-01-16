1b393b18ea5bb462e7770a606c3bbf386e2d2185
Jesus Zermeño <josezerrod@hotmail.com>
2024-01-16 12:57:20 -0600

    se agrega footer

---
+++ src/pages/logos_partners.js
@@ -0,0 +1,14 @@
+const partners = [    
+    { link: 'https://www.industriaacuicola.com/nueva_version/', src: '/worldseafood/img/industria_agricola.webp', alt:'Industria acuícola', width: 200, height: 65},
+    { link: 'http://divulgacionacuicola.com.mx/', src: '/worldseafood/img/divulgacion_acuicola.webp', alt:'Divulgación Acuicola', width: 200, height: 124},
+    { link: 'https://tecnoagro.com.mx/', src: '/worldseafood/img/tecno_agro.webp', alt:'TecnoAgro', width: 200, height: 70 },
+    { link: 'https://www.agrosintesis.com/home', src: '/worldseafood/img/agrosintesis.webp', alt:'Agrosíntesis', width: 100, height: 100 },
+    { link: 'https://www.periodicomitierra.mx/', src: '/worldseafood/img/periodico_mi_tierra.webp', alt:'Periódico mi tierra', width: 200, height: 111 },
+    { link: 'https://tierrafertil.com.mx/', src: '/worldseafood/img/tierra_fertil.webp', alt:'Tierra fertil multimedia agropecuaria', width: 200, height: 87 },
+    { link: 'https://greentology.life/', src: '/worldseafood/img/greentology.webp', alt:'Greentology', width: 200, height: 55 },
+    { link: 'https://globalindustries.mx/', src: '/worldseafood/img/globalindustries.webp', alt:'Global Industries', width: 200, height: 87 },
+    { link: 'https://dplnews.com/', src: '/worldseafood/img/dpl_news.webp', alt:'dpl news', width: 200, height: 76 },
+    { link: 'https://www.cosmos.com.mx/', src: '/worldseafood/img/cosmos.webp', alt:'Online Cosmos plataforma digital b2b', width: 200, height: 88 },
+    { link: 'https://www.horticultivos.com/home', src: '/worldseafood/img/horticultivos.webp', alt:'Horticultivos', width: 100, height: 100 },
+]
+export {partners}
