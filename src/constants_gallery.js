8b0b0c417f9a7f189c20c358ce511b509393e30b
Jesus Zermeño <josezerrod@hotmail.com>
2024-01-22 10:10:21 -0600

    se maqueta seccion de galeria

--- src/constants_gallery.js
+++ src/constants_gallery.js
@@ -1,5 +1,5 @@
 const toolkit_es = [
-    { id: 0, src: '/worldseafood/img/facebook_es.webp', alt:'Banner web', title:'Banner web', width: 360, height: 225 },
+    { id: 0, src: '/worldseafood/img/facebook_es.webp', alt:'Banner web', title:'Banner web', width: 360},
     { id: 1, src: '/worldseafood/img/keyvisual_worldseafood_es.webp', alt:'KeyVisual', title:'KeyVisual', width: 360, height: 158 },
     { id: 2, src: '/worldseafood/img/facebook_es.webp', alt:'Facebook', title:'Facebook', width: 360, height: 158 },
     { id: 3, src: '/worldseafood/img/twitter_es.webp', alt:'Twitter', title:'Twitter', width: 360, height: 120 },
@@ -7,11 +7,34 @@ const toolkit_es = [
 ]
 
 const toolkit_en = [
-    { id: 0, src: '/worldseafood/img/facebook_en.webp', alt:'Banner web', title:'Banner web', width: 360, height: 225 },
+    { id: 0, src: '/worldseafood/img/facebook_en.webp', alt:'Banner web',  title:'Banner web', width: 360},
     { id: 1, src: '/worldseafood/img/keyvisual_worldseafoodl_en.webp', alt:'KeyVisual', title:'KeyVisual', width: 360, height: 158 },
     { id: 2, src: '/worldseafood/img/facebook_en.webp', alt:'Facebook', title:'Facebook', width: 360, height: 158 },
     { id: 3, src: '/worldseafood/img/twitter_en.webp', alt:'Twitter', title:'Twitter', width: 360, height: 120 },
     { id: 4, src: '/worldseafood/img/linkedln_en.webp', alt:'Linkedin', title:'Linkedin', width: 360, height: 90 }    
 ]
+ const gallery2023 =[
+    { id: 0, src: '/worldseafood/img/gallery/gallery_1.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_2.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_3.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_4.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_5.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_6.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_7.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_8.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_9.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_10.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_11.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_12.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_13.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_14.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_15.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_16.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_17.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_18.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_19.jpg', alt:'Banner web', width: 360},
+    { id: 0, src: '/worldseafood/img/gallery/gallery_20.jpg', alt:'Banner web', width: 360},
+    
+ ]
 
-export {toolkit_es, toolkit_en}
+export {toolkit_es, toolkit_en, gallery2023}
