38a063b4cad2fc78c5b7049d8afd0573801fc6bb
Jesus Zermeño <josezerrod@hotmail.com>
2024-01-26 13:47:25 -0600

    se actualiza ruta de MEDIA PARTNERS

--- src/logos_partners.js
+++ src/logos_partners.js
@@ -1,24 +1,24 @@
 const partners = [    
-    { link: 'https://www.industriaacuicola.com/nueva_version/', src: '/img/industria_agricola.webp', alt:'Industria acuícola', width: 200, height: 65},
-    { link: 'http://divulgacionacuicola.com.mx/', src: '/img/divulgacion_acuicola.webp', alt:'Divulgación Acuicola', width: 200, height: 124},
-    { link: 'https://tecnoagro.com.mx/', src: '/img/tecno_agro.webp', alt:'TecnoAgro', width: 200, height: 70 },
-    { link: 'https://www.agrosintesis.com/home', src: '/img/agrosintesis.webp', alt:'Agrosíntesis', width: 100, height: 100 },
-    { link: 'https://www.periodicomitierra.mx/', src: '/img/periodico_mi_tierra.webp', alt:'Periódico mi tierra', width: 200, height: 111 },
-    { link: 'https://tierrafertil.com.mx/', src: '/img/tierra_fertil.webp', alt:'Tierra fertil multimedia agropecuaria', width: 200, height: 87 },
-    { link: 'https://greentology.life/', src: '/img/greentology.webp', alt:'Greentology', width: 200, height: 55 },
-    { link: 'https://globalindustries.mx/', src: '/img/globalindustries.webp', alt:'Global Industries', width: 200, height: 87 },
-    { link: 'https://dplnews.com/', src: '/img/dpl_news.webp', alt:'dpl news', width: 200, height: 76 },
-    { link: 'https://www.cosmos.com.mx/', src: '/img/cosmos.webp', alt:'Online Cosmos plataforma digital b2b', width: 200, height: 88 },
-    { link: 'https://www.horticultivos.com/home', src: '/img/horticultivos.webp', alt:'Horticultivos', width: 100, height: 100 },
-    { link: 'https://www.mmradio.com/', src: '/img/expositores/multimedios.webp', alt:'seaflyingfood', width: 220}
+    { link: 'https://www.industriaacuicola.com/nueva_version/', src: '/img/partners/industria_agricola.webp', alt:'Industria acuícola', width: 200, height: 65},
+    { link: 'http://divulgacionacuicola.com.mx/', src: '/img/partners/divulgacion_acuicola.webp', alt:'Divulgación Acuicola', width: 200, height: 124},
+    { link: 'https://tecnoagro.com.mx/', src: '/img/partners/tecno_agro.webp', alt:'TecnoAgro', width: 200, height: 70 },
+    { link: 'https://www.agrosintesis.com/home', src: '/img/partners/agrosintesis.webp', alt:'Agrosíntesis', width: 100, height: 100 },
+    { link: 'https://www.periodicomitierra.mx/', src: '/img/partners/periodico_mi_tierra.webp', alt:'Periódico mi tierra', width: 200, height: 111 },
+    { link: 'https://tierrafertil.com.mx/', src: '/img/partners/tierra_fertil.webp', alt:'Tierra fertil multimedia agropecuaria', width: 200, height: 87 },
+    { link: 'https://greentology.life/', src: '/img/partners/greentology.webp', alt:'Greentology', width: 200, height: 55 },
+    { link: 'https://globalindustries.mx/', src: '/img/partners/globalindustries.webp', alt:'Global Industries', width: 200, height: 87 },
+    { link: 'https://dplnews.com/', src: '/img/partners/dpl_news.webp', alt:'dpl news', width: 200, height: 76 },
+    { link: 'https://www.cosmos.com.mx/', src: '/img/partners/cosmos.webp', alt:'Online Cosmos plataforma digital b2b', width: 200, height: 88 },
+    { link: 'https://www.horticultivos.com/home', src: '/img/partners/horticultivos.webp', alt:'Horticultivos', width: 100, height: 100 },
+    { link: 'https://www.mmradio.com/', src: '/img/partners/multimedios.webp', alt:'Multimedmios', width: 220}
 ]
 
 const exhibitors = [    
     { link: 'https://mx.kaeser.com/', src: '/img/expositores/kaeser.webp', alt:'kaeser-compresores', width: 220},
-    { link: 'https://pulmex.com/', src: '/img/expositores/Pulmex2000.webp', alt:'pulmex', width: 220},
-    { link: 'https://oxipez.com/', src: '/img/expositores/oxipez.webp', alt:'oxipez', width: 220},
-    { link: 'http://www.transmarina.com/', src: '/img/expositores/transmarina.webp', alt:'transmarina', width: 220 },
-    { link: 'http://www.seaflyingfood.cn/page-35982.html', src: '/img/expositores/seaflyingfood.webp', alt:'seaflyingfood', width: 220}
+    { link: 'https://pulmex.com/', src: '/img/expositores/Pulmex2000.webp', alt:'Pulmex2000', width: 220},
+    { link: 'https://oxipez.com/', src: '/img/expositores/oxipez.webp', alt:'Oxipez', width: 220},
+    { link: 'http://www.transmarina.com/', src: '/img/expositores/transmarina.webp', alt:'Transmarina', width: 220 },
+    { link: 'http://www.seaflyingfood.cn/page-35982.html', src: '/img/expositores/seaflyingfood.webp', alt:'Seaflyingfood', width: 220}
 ]
 
 export {partners, exhibitors}
