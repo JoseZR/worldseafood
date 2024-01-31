90973bd08b50715bddf7ff12d6a6cb41da8c96a5
Pachi-Dev <pachi.claros@gmail.com>
2024-01-31 09:06:36 -0600

    Add Panorama Acuícola logo to partners

--- src/logos_partners.js
+++ src/logos_partners.js
@@ -10,7 +10,8 @@ const partners = [
     { link: 'https://dplnews.com/', src: '/img/partners/dpl_news.webp', alt:'dpl news', width: 200, height: 76 },
     { link: 'https://www.cosmos.com.mx/', src: '/img/partners/cosmos.webp', alt:'Online Cosmos plataforma digital b2b', width: 200, height: 88 },
     { link: 'https://www.horticultivos.com/home', src: '/img/partners/horticultivos.webp', alt:'Horticultivos', width: 100, height: 100 },
-    { link: 'https://www.mmradio.com/', src: '/img/partners/multimedios.webp', alt:'Multimedmios', width: 220}
+    { link: 'https://www.mmradio.com/', src: '/img/partners/multimedios.webp', alt:'Multimedmios', width: 220, height: 98 },
+    { link: 'https://panoramaacuicola.com/', src: '/img/partners/panorama-acuicola.webp', alt:'Panorama Acuícola', width: 200, height: 62 },
 ]
 
 const exhibitors = [    
